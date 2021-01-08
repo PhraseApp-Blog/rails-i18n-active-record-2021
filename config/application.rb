7fca1b4c457a60da0db0369d3fd08b24a54d9e1d
Mohammad Ashour <ashour.mohammad@gmail.com>
2021-01-08 13:05:46 +0100

    Installed Bulma and Bulmaswatch theme

---
+++ config/application.rb
@@ -0,0 +1,13 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module RailsI18nActiveRecord
+  class Application < Rails::Application
+    config.load_defaults 6.1
+  end
+end
